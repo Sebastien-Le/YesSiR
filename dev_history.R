4d4ffd3b923c474a42bfed1aac3dbbb14e320e93
Sebastien-Le <sebastien.le@agrocampus-ouest.fr>
2020-08-05 23:18:30 +0200

    Github

--- dev_history.R
+++ dev_history.R
@@ -1,4 +1,5 @@
 library(devtools)
+install.packages("goodpractice")
 usethis::use_git()
 use_r(name = "Yes_MCA")
 load_all()
@@ -13,3 +14,13 @@ use_tidy_description()
 attachment::att_to_description()
 
 check()
+
+goodpractice::goodpractice()
+
+install()
+
+use_github()
+
+#  57748b7b4441187694e3e97b4cf8f20e39a5ebb8
+
+
