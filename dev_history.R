92e7fe23a986d4356ed82a2dd1d2a7ba1f99325b
Sebastien-Le <sebastien.le@agrocampus-ouest.fr>
2020-08-05 22:28:58 +0200

    rev

--- dev_history.R
+++ dev_history.R
@@ -4,7 +4,12 @@ use_r(name = "Yes_MCA")
 load_all()
 document()
 
+#use_build_ignore("dev_history.R")
+
+#use_gpl3_license("Sébastien Lê")
 
 use_tidy_description()
 
 attachment::att_to_description()
+
+check()
